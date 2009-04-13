62cdacaaacb535408975db946293a5d5e0851a96
Matt Sanford <matt@twitter.com>
2009-04-13 12:13:36 -0700

    Fix incorrect wording pointed out by guanix

--- test/test_signature.rb
+++ test/test_signature.rb
@@ -8,9 +8,9 @@ class TestOauth < Test::Unit::TestCase
       %w(n N e E s S u U).each do |kcode|
         $KCODE = kcode
         assert_equal '%E3%81%82', OAuth::Helper.escape('あ'),
-                      "Failed to correctly Japanese escape under $KCODE = #{kcode}"
+                      "Failed to correctly escape Japanese under $KCODE = #{kcode}"
         assert_equal '%C3%A9', OAuth::Helper.escape('é'),
-                      "Failed to correctly Japanese escape under $KCODE = #{kcode}"
+                      "Failed to correctly escape e+acute under $KCODE = #{kcode}"
       end
     ensure
       $KCODE = old
